900a138c544723e10dc4f3892c748a6842bf4f3f
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-07-21 15:50:48 +0200

    merged and addes server.js (WIP)

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import "./config/config.js"; //als 1. importieren!!!
-import express from "express";
+import express, { application } from "express";
 import "./models/index.js"; // = running index.js
 import multer from "multer";
+
+//* Models importieren
+import { BigStuff } from "./models/BigStuffModel.js";
+import { NotSoBigStuff } from "./models/NotSoBigStuffModel.js";
+import { SmallStuff } from "./models/SmallStuff.js";
 
 //* CLOUDINARY Config
 import { v2 as cloudinary } from "cloudinary";
@@ -21,13 +26,107 @@ app.use(express.json());
 
 //! ------------ GET ROUTES ------------------------
 //- BigStuff "fetchen"
+app.get("/api/bigstuff", async (req, res) => {
+	try {
+		const allBigStuff = await BigStuff.find();
+		res.send(allBigStuff);
+	} catch (err) {
+		console.error(err);
+		res.send("There was an error fetching the Big Stuff");
+	}
+});
 //- NotSoBigStuff "fetchen"
+app.get("/api/notsobigstuff", async (req, res) => {
+	try {
+		const allBigStuff = await NotSoBigStuff.find();
+		res.send(allBigStuff);
+	} catch (err) {
+		console.error(err);
+		res.send("There was an error fetching the Big Stuff");
+	}
+});
 //- SmallStuff "fetchen"
+app.get("/api/smallstuff", async (req, res) => {
+	try {
+		const allBigStuff = await SmallStuff.find();
+		res.send(allBigStuff);
+	} catch (err) {
+		console.error(err);
+		res.send("There was an error fetching the Big Stuff");
+	}
+});
 
 //! ------------ POST ROUTES ------------------------
 //- BigStuff hinzufuegen
+app.post("/api/bigstuff", upload.single("image"), async (req, res) => {
+	console.log(req.file);
+	try {
+		cloudinary.uploader
+			.upload_stream(
+				{
+					resource_type: "image",
+					folder: "FurnitureBig",
+				},
+				async (err, result) => {
+					const response = await BigStuff.create({
+						...req.body,
+						image: { url: result.secure_url, imageId: result.public_id },
+					});
+					res.send(response);
+				},
+			)
+			.end(req.file.buffer);
+	} catch (err) {
+		console.error(err);
+	}
+});
+
 //- NotSoBigStuff hinzufuegen
+app.post("/api/notsobigstuff", upload.single("image"), async (req, res) => {
+	console.log(req.file);
+	try {
+		cloudinary.uploader
+			.upload_stream(
+				{
+					resource_type: "image",
+					folder: "FurnitureNotSoBig",
+				},
+				async (err, result) => {
+					const response = await NotSoBigStuff.create({
+						...req.body,
+						image: { url: result.secure_url, imageId: result.public_id },
+					});
+					res.send(response);
+				},
+			)
+			.end(req.file.buffer);
+	} catch (err) {
+		console.error(err);
+	}
+});
 //- SmallStuff hinzufuegen
+app.post("/api/smallstuff", upload.single("image"), async (req, res) => {
+	console.log(req.file);
+	try {
+		cloudinary.uploader
+			.upload_stream(
+				{
+					resource_type: "image",
+					folder: "FurnitureSmall",
+				},
+				async (err, result) => {
+					const response = await SmallStuff.create({
+						...req.body,
+						image: { url: result.secure_url, imageId: result.public_id },
+					});
+					res.send(response);
+				},
+			)
+			.end(req.file.buffer);
+	} catch (err) {
+		console.error(err);
+	}
+});
 
 //! ------------ PUT ROUTES ------------------------
 //- BigStuff editieren
@@ -38,3 +137,5 @@ app.use(express.json());
 //- BigStuff loeschen
 //- NotSoBigStuff loeschen
 //- SmallStuff loeschen
+
+app.listen(PORT, () => console.log(`Server is running on: ${PORT}`));
