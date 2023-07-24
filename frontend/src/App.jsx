74baa5fb60a3590e95cde223c9e16ece2e2dd10f
Roman Parfentev <romanntev@gmail.com>
2023-07-24 11:49:18 +0200

    add loading screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,30 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { refreshContext, loadingContext } from "./context/Context";
 import Home from "./pages/Home/Home";
 import BigStuff from "./pages/BigStuff/BigStuff";
 import NotSoBigStuff from "./pages/NotSoBigStuff/NotSoBigStuff";
 import SmallStuff from "./pages/SmallStuff/SmallStuff";
-import { useState } from "react";
-import { refreshContext } from "./context/Context";
 
 function App() {
   const [refresh, setRefresh] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
-      <refreshContext.Provider value={{ refresh, setRefresh }}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/bigstuff" element={<BigStuff />} />
-            <Route path="/notsobigstuff" element={<NotSoBigStuff />} />
-            <Route path="/smallstuff" element={<SmallStuff />} />
-          </Routes>
-        </BrowserRouter>
-      </refreshContext.Provider>
+      <loadingContext.Provider value={{ loading, setLoading }}>
+        <refreshContext.Provider value={{ refresh, setRefresh }}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/bigstuff" element={<BigStuff />} />
+              <Route path="/notsobigstuff" element={<NotSoBigStuff />} />
+              <Route path="/smallstuff" element={<SmallStuff />} />
+            </Routes>
+          </BrowserRouter>
+        </refreshContext.Provider>
+      </loadingContext.Provider>
     </>
   );
 }
--- frontend/src/pages/NotSoBigStuff/NotSoBigStuff.jsx
+++ frontend/src/pages/NotSoBigStuff/NotSoBigStuff.jsx
@@ -1,11 +1,32 @@
 import "./NotSoBigStuff.css";
+import { useState } from "react";
+import Nav from "../../components/Nav/Nav";
+import AddButton from "../../components/AddButton/AddButton";
+import AddNewItem from "../../components/AddNewItem/AddNewItem";
+import DetailCard from "../../components/DetailCard/DetailCard";
 
 const NotSoBigStuff = () => {
-	return (
-		<>
-			<h1>NotSoBigStuff</h1>
-		</>
-	);
+  const [formIsActive, setFormIsActive] = useState(false);
+
+  const stuffCategory = "notsobigstuff";
+
+  return (
+    <>
+      <Nav />
+      <main>
+        <AddButton
+          formIsActive={formIsActive}
+          setFormIsActive={setFormIsActive}
+        />
+        <AddNewItem
+          formIsActive={formIsActive}
+          setFormIsActive={setFormIsActive}
+          stuffCategory={stuffCategory}
+        />
+        <DetailCard stuffCategory={stuffCategory} />
+      </main>
+    </>
+  );
 };
 
 export default NotSoBigStuff;
