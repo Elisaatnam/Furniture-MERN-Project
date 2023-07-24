f6ec77030acc2e9dbc5d9edbfc7c634de98df880
ElisaStueker <128373269+Elisaatnam@users.noreply.github.com>
2023-07-24 15:07:18 +0200

    added new routes in server.js

--- frontend/src/components/DetailCard/DetailCard.jsx
+++ frontend/src/components/DetailCard/DetailCard.jsx
@@ -4,31 +4,31 @@ import axios from "axios";
 import { refreshContext } from "../../context/Context";
 
 const DetailCard = ({ category, elm }) => {
-  const { refresh, setRefresh } = useContext(refreshContext);
+	const { refresh, setRefresh } = useContext(refreshContext);
 
-  const handleDelete = async (stuffId) => {
-    try {
-      const { data } = await axios.delete(`/api/${category}/${stuffId}`);
-    } catch (error) {
-      console.log("handleDelete: ", error);
-    }
-    setRefresh((prev) => !prev);
-  };
+	const handleDelete = async stuffId => {
+		try {
+			const { data } = await axios.delete(`/api/${category}/${stuffId}`);
+		} catch (error) {
+			console.log("handleDelete: ", error);
+		}
+		setRefresh(prev => !prev);
+	};
 
-  return (
-    <div className="detailCard">
-      <img src={elm.image.url} alt={elm.title} />
-      <div className="content">
-        <h2>{elm.title}</h2>
-        <h3>{elm.room}</h3>
-        <p>{elm.content}</p>
-      </div>
-      <div className="buttons">
-        <button onClick={() => handleDelete(elm._id)}>Delete</button>
-        <button>To Page</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className='detailCard'>
+			<img src={elm.image.url} alt={elm.title} />
+			<div className='content'>
+				<h2>{elm.title}</h2>
+				<h3>{elm.room}</h3>
+				<p>{elm.content}</p>
+			</div>
+			<div className='buttons'>
+				<button onClick={() => handleDelete(elm._id)}>Delete</button>
+				<button>To Page</button>
+			</div>
+		</div>
+	);
 };
 
 export default DetailCard;
